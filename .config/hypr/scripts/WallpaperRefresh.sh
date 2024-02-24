50aef95219023fa2d86fb6dddc12115685a8f21a
Alex Reyes <alex@alexreyes.com>
2024-02-24 08:56:51 -0600

    u - current hyprland configuration

---
+++ .config/hypr/scripts/WallpaperRefresh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPTSDIR=$HOME/.config/hypr/scripts
+
+# Kill already running process
+#_ps=(waybar dunst rofi)
+_ps=(rofi)
+for _prs in "${_ps[@]}"; do
+	if [[ $(pidof ${_prs}) ]]; then
+		pkill ${_prs}
+	fi
+done
+
+# Lauch notification daemon (dunst)
+# ${SCRIPTSDIR}/Dunst.sh &
+
+# Lauch statusbar (waybar)
+# ${SCRIPTSDIR}/Waybar.sh &
