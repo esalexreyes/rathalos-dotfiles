815390a90f8b66083328b97cc49b3b7d93a9e929
Alex Reyes <alex@alexreyes.com>
2024-04-04 23:27:54 -0600

    u - Add nvim-tmux-navigator

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -5,3 +5,12 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " "
 vim.o.termguicolors = true
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
