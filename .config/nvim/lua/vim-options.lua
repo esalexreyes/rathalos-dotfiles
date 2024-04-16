453584fd257ff3899e76f030cc76a4d387757583
Alex Reyes <alex@alexreyes.com>
2024-04-16 14:55:10 -0600

    u - add descriptions to commands

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -7,10 +7,10 @@ vim.g.mapleader = " "
 vim.o.termguicolors = true
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', { desc = "Move to window above"})
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>', { desc = "Move to window below"})
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>', { desc = "Move to window left"})
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>', { desc = "Move to window right"})
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
