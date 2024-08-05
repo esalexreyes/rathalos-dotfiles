23d3ec4be47e0c0743aa733ae598de8bc85f63c5
Alex Reyes <alex@alexreyes.com>
2024-08-05 08:58:06 -0600

    c - Add services for swww

---
+++ .config/swww/swww.sh
@@ -0,0 +1,5 @@
+!/usr/bin/bash
+#start swww
+WALLPAPERS_DIR=~/Pictures/Wallpapers
+WALLPAPER=$(find "$WALLPAPERS_DIR" -type f | shuf -n 1)
+swww img "$WALLPAPER" --resize fit --transition-step 90 --transition-fps 60 --transition-type any --transition-duration 3
